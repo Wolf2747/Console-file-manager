8b17ec1e1bb61fda3f54f6946a60fb2a709b84de
progress <romanbor2005@gmail.com>
2022-09-24 17:12:36 +0300

    тесты

--- test_python.py
+++ test_python.py
@@ -1,4 +1,10 @@
-
-
 def test_filters():
-   new_numbers = filter(lambda x: x%2 == 0,[1,2,3,4,5,6,7,8,9,10])
+   new_numbers = list(filter(lambda x: x%2 == 0,[1,2,3,4,5,6,7,8,9,10]))
+   assert new_numbers == [2,4,6,8,10]
+def test_map():
+   new_list = list(map(int,['1','2','3','4','5']))
+   assert new_list == [1,2,3,4,5]
+def test_sort():
+   numbers = [1,25,-9,56,-128]
+   numbers.sort()
+   assert numbers == [-128,-9,1,25,56]
